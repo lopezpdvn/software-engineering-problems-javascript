af5e5b77015ae7daff8a2524b3d199fa35926fa9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-11-24 05:46:09 -0600

    Added dummy tests and reference to src project

--- src/SoftwareEngineeringProblemsJavaScript/app.js
+++ src/SoftwareEngineeringProblemsJavaScript/app.js
@@ -1,1 +1,3 @@
-﻿console.log('Hello world');
+﻿module.exports = function () {
+    console.log('a function in file misc');
+};
---
+++ tests/SoftwareEngineeringProblemsJavaScript.Tests/Problems/Misc.js
@@ -0,0 +1,24 @@
+﻿var assert = require('assert');
+var foo = require('../../../src/SoftwareEngineeringProblemsJavaScript');
+
+describe('Test Suite 1', function () {
+    it('Test 1', function () {
+        assert.ok(true, "This shouldn't fail");
+    });
+    
+    it('Test 2', function () {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    });
+});
+
+describe('Test Suite 2', function () {
+    it('Test 3', function () {
+        assert.ok(true, "This shouldn't fail");
+    });
+    
+    it('Test 4', function () {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    });
+})
