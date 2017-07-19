7d8e480c2970be6e1ab73bb2a4f101c3b8b5fd0e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2017-07-18 19:30:06 -0500

    Spread operator as a better `Function.prototype.apply`

--- test/lib/misc/patterns.js
+++ test/lib/misc/patterns.js
@@ -3,7 +3,6 @@
 const assert = require('assert');
 
 describe('Patterns', function() {
-
     it('Iterator low level', function() {
         const x = ['zero', 'one', 'two', 'three'];
         const ans = x.slice();
@@ -48,4 +47,11 @@ describe('Patterns', function() {
         assert.deepStrictEqual(correctSequence, [...mainGenerator()]);
     });
 
+    it('Spread operator as a better `Function.prototype.apply`', function() {
+        const a = [-1, 2, 1, 0, 3, -3];
+        assert.ok(3 === Math.max.apply(null, a));
+        assert.ok(3 === Math.max(...a));
+        assert.ok(-3 === Math.min.apply(null, a));
+        assert.ok(-3 === Math.min(...a));
+    });
 });
